1e6e15eaebae689e03c777bbc2a52d7c2ac2717e
Samyog <samyog.dc@gmail.com>
2024-08-31 14:18:48 +1000

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/samyogPortfolio/', // Set this to your GitHub Pages repository name
   plugins: [react()],
 })
